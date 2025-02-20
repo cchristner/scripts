2f6fc4f6a44ae715d595db67d41f173663c7d9ee
Corey <cchristner91@gmail.com>
2025-02-20 00:12:55 -0500

    notes

--- doc_search.py
+++ doc_search.py
@@ -3,6 +3,18 @@ import re
 import docx2txt
 from glob import glob
 
+"""
+I use this to crawl my .docx files I use for writing and note taking for keywords or phrases.
+
+Works well for now, but not too many files.
+
+Will probably need to think about this if scale becomes too much to sift through.
+
+To Do:
+1. Make the files return in a way that makes them easily searched for, or even clickable. Maybe use the numbered list it returns, and then gives another prompt for the user to input one or multiple numbers and the script will auto open them?
+"""
+
+
 def search_word_docs(keyword):
     """
     Search all Word documents in the current directory for a keyword/phrase.
